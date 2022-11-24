e63ecba0e631af28aa478c2fc00da1c4aff1ef38
annt <annt@gft.com>
2022-11-24 15:49:11 +0000

    Done with trim(), for, StringBuilder and a replaceAll

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,24 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+    public Solution(){
+
+    }
+
+    public String reverseWords(String s) {
+
+        String result;
+        StringBuilder aux = new StringBuilder();
+        String[] listWords =  s.trim().replaceAll("\\s+", " ").split(" ");
+
+
+       for (int i = listWords.length - 1; i >= 0; i--){
+
+           aux.append(listWords[i]).append(" ");
+       }
+
+       result = aux.toString().trim();
+
+        return result;
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,69 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+    String result, s;
+
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void EmptyString(){
+
+        result = "";
+        s = "";
+
+        assertEquals(result, solution.reverseWords(s));
+
+    }
+
+    @Test
+    public void OneWordString(){
+
+        result = "house";
+        s = "house";
+
+        assertEquals(result, solution.reverseWords(s));
+
+    }
+
+    @Test
+    public void TwoWordString(){
+
+        result = "house new";
+        s = "new house";
+
+        assertEquals(result, solution.reverseWords(s));
+
+    }
+
+    @Test
+    public void SevenWordString(){
+
+        result = "want I what is house new A";
+        s = "A new house is what I want";
+
+        assertEquals(result, solution.reverseWords(s));
+
+    }
+
+    @Test
+    public void SevenWordStringWithSpacesBetweenWords(){
+
+        result = "want I what is house new A";
+        s = "A  new  house     is what    I want";
+
+        assertEquals(result, solution.reverseWords(s));
+
+    }
 }
